63a08738699b25e502b15f6269a7ba24d95f531b
Quentin Doulcet <quentinucme@hotmail.fr>
2024-02-15 00:51:59 +0100

    j'essaye des choses

---
+++ script.js
@@ -0,0 +1,19 @@
+var nombres = 3
+console.log(nombres)
+
+var date_aujourdhui = new Date
+console.log(date_aujourdhui)
+
+var date = date_aujourdhui.getDate()
+console.log(date)
+
+var date = date_aujourdhui.getMonth()
+console.log(date)
+
+document.write(date)
+
+var chiffre = 0
+
+while (chiffre < 15) {
+  chiffre++
+}
